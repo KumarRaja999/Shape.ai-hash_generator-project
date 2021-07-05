e53b8b797b9b18e78b5a149c39383637a0544463
KumarRaja999 <86291719+KumarRaja999@users.noreply.github.com>
2021-07-05 17:27:36 +0530

    Add files via upload

---
+++ hash_generator.py
@@ -0,0 +1,9 @@
+# Python 3 code to demonstrate the
+# working of MD5 (byte - byte)
+import hashlib
+# encoding GeeksforGeeks using md5 hash
+# function
+result = hashlib.md5(b'immanuel')
+# printing the equivalent byte value.
+print("The byte equivalent of hash is : ", end ="")
+print(result.digest())
